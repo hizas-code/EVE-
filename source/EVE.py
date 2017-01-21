e552d415a8d894bc398834554ef199f899fa34a1
firdaaminy <firda_maruf@yahoo.com>
2017-01-21 08:04:03 +0700

    FIRDA PATTERN BARU

--- source/EVE.py
+++ source/EVE.py
@@ -1,4 +1,4 @@
-gitimport re
+import re
 import random
 
 reflections ={
@@ -17,69 +17,74 @@ reflections ={
     "me": "you"}
 
 pattern = [
-    [r'(Hello|Hi)',
+    [r'(Hello|Hi|Hey)',
      ["{0}"]],
 
     [r'How are you ([^\?]*)\??',
      [" I'm fine {0} thank you, how about you?"]],
 
-    [r' (.*) you like football?',
+    [r'I\'?m (fine|good|happy|ok)',
+     [" I'm so glad you are {0}. Let's talk about football"]],
+
+    [r'(.*)fuck|shit|damn|asshole(.*)?',
+     ["That's a bad word to say",
+      "Hey hey, language please",
+      "Don't make me tell your mother!"]],
+
+    [r'I\'?m (sad|stress|tired|)',
+     ["Maybe talking about football will cheer you up!",
+      "This is not the day for you to feel {0}",
+      "Oh come on, don't waste your time to feel {0}",
+      "Why are you {0}?"]],
+
+    [r'(.*)you like football?',
      ["I don't watch it very often but I do have the knowledge of it.",
       "If you give me questions about it, I can answer it directly.",
       "Yeah of course, who doesn't like?",
       "If you like it, then I like it as well ;)"]],
 
-    [r' When was football (created|found|invented|started)? ',
+    [r'When was football (created|found|invented|started)? ',
      ["Football was {0} by Ancient Greeks (400-375 BC) and then Chinese added rules to it (1130-1160)."]],
 
-    [r' (.*) coach of (real madrid|Real Madrid|realmadrid|real Madrid|Real madrid)?',
-     ["Coach of {0} is Zinédine Zidane."]],
-
-    [r' (.*) coach of (bayern mucnhen|Bayern Munchen|bayernmunchen|bayern Munchen|Bayern munchen)?',
-     ["Coach of {0} is Carlo Ancelotti."]],
-
-    [r' (.*) coach of (barcelona|Barcelona)?',
-     ["Coach of {0} is Luis Enrique Martínez García."]],
-
-    [r' (.*) coach of (juventus|Juventus)?',
-     ["Coach of {0} is Massimiliano Allegri."]],
-
-    [r' (.*) coach of (Manchester United|manchester united|manchesterunited|manchester United|Manchester united)?',
-     ["Coach of {0} is Jose Mourinho."]],
-
-    [r' I (like|love) football',
+    [r'I (like|love) football',
      ["Hmm what do you {0} about it?",
       "Can you tell me the reasons why you {0} it?",
       "How does football make you feel? ",
       "Well who doesn't!.",
       "I see, football does bring people together."]],
 
-    [r' I (hate|despise| am sick of) football',
+    [r'I (hate|despise| am sick of) football',
      ["Hmm what do you {0} about it?",
       "Can you tell me the reasons why you {0} it?",
       "Well, that escalated quickly!",
       "I see, there are some people who {0} football.",
       "Is this pure hatred or just because you don't understand about it?"]],
 
-    [r' (.*) football (.*)',
+    [r'Because(.*)',
      ["Hmm I see, tell me more about it.",
-      "I never heard that before. Please, fascinate me more!",
       "That's quite interesting.",
-      "Why do you say that?",
       "I totally get it",
-      "Would you be kind enough to elaborate it?",
-      "Does anyone ever tell you that you have such an amazing way of thinking?",
-      "IKR!"]],
+      "Would you be kind enough to elaborate it?"]],
 
-    [r' You (.*)?',
-     ["We're going far off the topic here.",
-      "Why do you ask that?",
-      "I can only answer question about football, remember?",
+    [r'I think(.*)football(.*)',
+     ["I never heard that before. Please, explain to me me more!",
+      "Why do you say that?",
+      "There are some people who doesn't think that way"]],
+
+    [r'Why(.*)?',
+     ["Why do you ask that?",
+      "Good question. Now give me another one about football!",
       "Maybe the answer lies within yourself.",
       "I'm a bot, I don't have every answer to every question. I'm sorry",
       "Maybe next time I'll answer that when my creators upgrade me"]],
 
-    [r' I (like|love) team (.*)',
+    [r'Can I(.*)?',
+     ["Yes you can {0}!",
+      "Call me an optimist, but I believe you can {0}",
+      "Why don't yu try it first?"
+      ]],
+
+    [r'I (like|love) team(.*)',
      ["Hmm what do you {0} about them?",
       "Can you tell me the reasons why you {0} them?",
       "Oh, I like that team too! We're match made in heaven. ",
@@ -87,41 +92,42 @@ pattern = [
       "I see, are you a huge fan of them?",
       "They're one of the best team in this world."]],
 
-    [r' I (hate|despise|am sick of) team (.*)',
+    [r'(.*)favorite team(.*)',
+     ["Hmm what do you like about them?",
+      "Can you tell me the reasons why you they're your favorite?",
+      "Oh, that's my favorite team too! We're match made in heaven. ",
+      "Well who doesn't!.",
+      "I see, are you a huge fan of them?",
+      "They're one of the best team in this world."]],
+
+    [r'I (hate|despise|am sick of) team(.*)',
      ["Hmm what do you {0} about them?",
       "Can you tell me the reasons why you {0} them?",
       "Well, that escalated quickly!",
       "I'm sorry, I never hate on anything :(",
       "Is this pure hatred or just because your favorite team got beat by team {0}?",
-      "I see, but who knows, maybe you'll like them one day!"]],
+      "I see, but who knows, maybe you'll like team {0} one day!"]],
 
-    [r' I don\'?t  (.*) football (.*)',
+    [r'I don\'?t(.*)football(.*)',
      ["Oh really? Why?",
       "Could you explain why you don't {0}",
-      "Maybe if you try {0}, your statement will change",
+      "Maybe if you try it, your mind will change",
       "The question is: do you want to{0}?"]],
 
-    [r' You (.*)',
+    [r'You(.*)',
      ["We're going far off the topic here.",
       "Why do you say that?",
       "We should be discussing football remember?",
       "Let's get back on what you have to say or ask about football, shall we?",
       "Hmmmmm really? I never really thought about that myself."]],
 
-    [r' quit|bye|goodbye',
+    [r'quit|bye|goodbye',
      ["I have such a wonderful time talking to you. Goodbye!",
       "Goodbye, hope you have a good day!",
       "Thank you for talking with me. Hope you'll be back soon!",
       "Well g'day to you, Sir!",
       "Thank you, that will be $200......nah I'm just messing with you :))"
       ]],
-
-[r' (.*)',
-     ["We're going far off the topic here.",
-      "Why do you say that?",
-      "We should be discussing football remember?",
-      "Let's get back on what you have to say or ask about football, shall we?",
-      "Hmmmmm really? I never really thought about that myself."]],
 ]
 
 
